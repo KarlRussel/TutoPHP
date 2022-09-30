4e54b13dd42b791c2002b3b3a92849fb9e136b51
russel <karlrusselgbedjeglo@gmail.com>
2022-09-30 19:33:56 +0100

    introduction aux conditions

--- tableaux.php
+++ tableaux.php
@@ -7,6 +7,6 @@ $identite = [
 ];
 $identite['prenom'] = 'John'; // on a changé directement le prénom ou de manière globale, la valeur contenur dans la variable prenom
 $identite['notes'][] = 17; // on ajoute une nouvelle note au tableau contenant les notes
-print_r ($identite['notes']); // on utilie print_r pour afficher le contenu d'un tableau
+print_r ($identite['notes']); // on utilie print_r pour afficher tout le contenu d'un tableau
 
 ?>
