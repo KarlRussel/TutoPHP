f19a3a95abca030a11aa836d378c77fa6809729c
russel <karlrusselgbedjeglo@gmail.com>
2022-09-30 19:31:07 +0100

    introduction aux tableaux

---
+++ tableaux.php
@@ -0,0 +1,12 @@
+<?php
+$identite = [
+    'nom' => 'MINKA',
+    'prenom' => 'Jean',
+    'notes' => [12, 14, 07, 10]
+
+];
+$identite['prenom'] = 'John'; // on a changé directement le prénom ou de manière globale, la valeur contenur dans la variable prenom
+$identite['notes'][] = 17; // on ajoute une nouvelle note au tableau contenant les notes
+print_r ($identite['notes']); // on utilie print_r pour afficher le contenu d'un tableau
+
+?>
