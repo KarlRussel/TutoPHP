41fb5dc7b46eb0f3af5244665a10c147f2ad5d82
russel <karlrusselgbedjeglo@gmail.com>
2022-10-03 12:08:29 +0100

    progression sur les conditions

--- conditions.php
+++ conditions.php
@@ -1,3 +1,30 @@
 <?php
-
+/*
+$action = readline ("Chosissez une action entre : 1(attaquer),2(défendre) ou 3(passer son tour)");
+switch($action) {
+    case 1: 
+        echo "J'attaque";
+        break;
+    case 2:
+        echo "Je défends";
+        break;
+    case 3:
+        echo "Je passe mon tour";
+        break;
+    default:
+        echo "Commande inconnue";
+}
+Se rappeler de cette façon de faire quand on a plusieurs cas de figure distincts*/
+/*
+$moy= (int) readline ("Entrez votre moyenne: "); //Le readline permet à l'utilisateur de taper sa propre moyenne. le int en début permet à php de comprendre que la valeur tapée est de type entier. sinon par défaut, php considère tout ce qui est tapé comme une chaine de caractères
+    if ($moy > 10) {
+        echo "Vous avez la moyenne";
+    }
+    elseif ($moy === 10) { //Se rappeler de la différence qu'il y a entre un double et un triple égale
+        echo "Vous avez juste la moyenne";
+    }
+    else {
+        echo "Vous n'avez pas la moyenne";
+    }
+*/
 ?>
